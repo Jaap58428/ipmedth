a3ea181b2ebc59a54c012ae5a30c03597225cb79
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-05-25 14:00:45 +0200

    added aframe files to system structure including refferences

---
+++ js/vr_logic/movement.js
@@ -0,0 +1,132 @@
+const pathConnections = {
+  1: {
+    2: {
+      4: {
+        5: -1
+      }
+    },
+    3: {
+      4: {
+        5: -1
+      },
+      5: -1
+    }
+  }
+}
+
+getCoordinatesDistance = (oldPosition, newPosition) => {
+  let productX = Number(newPosition.x) - Number(oldPosition.x);
+  let productY = Number(newPosition.y) - Number(oldPosition.y);
+  let productZ = Number(newPosition.z) - Number(oldPosition.z);
+
+  return Math.sqrt((productX * productX) + (productY * productY) + (productZ * productZ))
+
+}
+
+checkFinish = () => {
+  console.log('animation ended');
+  
+}
+
+getMovementAnimation = (blockCoordinates) => {
+  
+  let animation = document.createElement('a-animation');
+  let oldPosition = document.getElementById('camera').getAttribute('position')
+  let newPosition = blockCoordinates.getAttribute('position')
+
+  let distance = getCoordinatesDistance(oldPosition, newPosition)
+
+  animation.setAttribute('attribute', 'position')
+  animation.setAttribute('from', `${oldPosition.x} ${oldPosition.y} ${oldPosition.z}`)
+  animation.setAttribute('to', `${newPosition.x} ${newPosition.y + 1  } ${newPosition.z}`)
+  animation.setAttribute('fill', 'forwards')
+  animation.setAttribute('easing', 'linear')
+  animation.setAttribute('dur', (distance * 300).toString());
+  animation.addEventListener('animationend', checkFinish)
+  return animation
+}
+
+makeTargetVisible = (element) => {
+  element.appendChild(document.getElementById('navAniHeight').cloneNode(true))
+  element.appendChild(document.getElementById('navAniColour').cloneNode(true))
+}
+
+setUserOnFirstCoordinate = (navElements) => {
+  for (let i = 0; i < navElements.length; i++) {
+    // if its a match: attach a clickListener
+    if (navElements[i].dataset.target === "1") {
+      document.getElementById('camera').setAttribute('position', navElements[i].getAttribute('position'))
+    }
+  }
+}
+
+startMovement = () => {
+  // get navigation nodes
+  let navElements = document.getElementById('navigationBox').children
+
+  // grab first target(s)
+  let targetIds = pathConnections[1]
+
+  setUserOnFirstCoordinate(navElements)
+
+  // remove old attributes so user can't make invalid moves
+  clearOldListeners = () => {
+    for (let i = 0; i < navElements.length; i++) {
+      navElements[i].removeEventListener('click', moveHere)
+      while (navElements[i].firstChild) {
+        navElements[i].removeChild(navElements[i].firstChild);
+      }
+      navElements[i].setAttribute('color', 'white')
+      navElements[i].setAttribute('height', '0.2')
+    }
+  }
+
+  setNewTargets = (element) => {
+    newTargetsIds = targetIds[key]
+    for (key in targetIds) {
+      if (targetIds.hasOwnProperty(key)) {
+        if (element.dataset.target === key) {
+          targetIds = targetIds[key]
+        }
+      }
+    }
+
+  }
+
+  // add movement animation to user's location at click
+  moveHere = (clickEvent) => {
+    document.getElementById('camera').appendChild(getMovementAnimation(clickEvent.target))
+    while (clickEvent.target.firstChild) {
+      clickEvent.target.removeChild(clickEvent.target.firstChild);
+    }
+    clearOldListeners()
+    setNewTargets(clickEvent.target)
+    addNewListeners()
+  }
+
+  // add listeners to the new possible targets
+  addNewListeners = () => {
+    let newTargetsIds;
+    // loop through new targetsId's
+    for (key in targetIds) {
+      if (targetIds.hasOwnProperty(key)) {
+
+        // for every id check it matches any nav element
+        for (let i = 0; i < navElements.length; i++) {
+
+          // if its a match: attach a clickListener
+          if (navElements[i].dataset.target === key) {
+            navElements[i].addEventListener('click', moveHere)
+            console.log('added listener: ', navElements[i]);
+            makeTargetVisible(navElements[i])
+          }
+        }
+      }
+    }
+  }
+
+  // add first listeners
+  addNewListeners()
+}
+
+startMovement();
---
+++ js/vr_logic/waves.js
@@ -0,0 +1,104 @@
+  const fillOcean = (ocean, oceanLength, oceanWidth, waveHeight, oceanResolution) => {
+      // cycle through columns and rows to fill ocean with elements
+      for (let row = 0; row < oceanLength; row++) {
+          for (let column = 0; column < oceanWidth; column++) {
+              let newOceanElement = getNewOceanElement(oceanResolution, waveHeight);
+              newOceanElement.setAttribute('color', getRandomBlueTint());
+              newOceanElement.setAttribute('position', {
+                  x: row * oceanResolution,
+                  y: 0 - waveHeight,
+                  z: (0 - column) * oceanResolution
+              });
+
+              // animation depends on getting the current location from canvas
+              // wait untill component is initialised
+              newOceanElement.addEventListener('loaded', () => {
+                  addAnimationToOceanElement(newOceanElement, row, column, waveHeight);
+              })
+
+              ocean.appendChild(newOceanElement);
+          }
+      }
+  }
+
+  const addAnimationToOceanElement = (oceanElement, row, column, waveHeight) => {
+      // build up new animation component
+      let animation = document.createElement('a-animation');
+      animation.setAttribute('attribute', 'position');
+      animation.setAttribute('dur', '4000');
+
+      let currentLocation = oceanElement.getAttribute('position');
+      let newLocation = {
+          x: currentLocation.x,
+          y: currentLocation.y + waveHeight,
+          z: currentLocation.z
+      }
+      animation.setAttribute('from', `${currentLocation.x} ${currentLocation.y} ${currentLocation.z}`);
+      animation.setAttribute('to', `${newLocation.x} ${newLocation.y} ${newLocation.z}`);
+
+      animation.setAttribute('direction', 'alternate');
+      animation.setAttribute('easing', 'ease-in');
+      animation.setAttribute('repeat', 'indefinite');
+      animation.setAttribute('delay', '1000');
+
+      // add animation to component based of diagonal 2d raster
+      setTimeout(() => {
+          oceanElement.appendChild(animation)
+      }, (row * 1.4 + column * 0.6) * 500);
+  }
+
+  const getRandomBlueTint = () => {
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#HSL_colors
+      const hue = Math.round((Math.random() * 20)) + 200;
+      const saturation = Math.round((Math.random() * 50)) + 50;
+      const lightness = Math.round((Math.random() * 50)) + 25;
+
+      return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
+
+  const getNewOceanElement = (oceanResolution, waveHeight) => {
+      // generate a standard ocean element
+      let oceanElement = document.createElement('a-box');
+      oceanElement.setAttribute('depth', oceanResolution.toString());
+      oceanElement.setAttribute('width', oceanResolution.toString());
+
+      // always add a bit more height then animation needs
+      // this avoids potential clipping
+      oceanElement.setAttribute('height', (waveHeight + 0.05).toString());
+
+      oceanElement.className = "oceanElement"
+
+      return oceanElement;
+  }
+
+  const main = () => {
+      // grab ocean component to fill with elements
+      let ocean = document.getElementById('ocean');
+
+      // set global ocean variables
+      let oceanLength = parseInt(ocean.getAttribute('data-oceanLength')); // how many elements long the ocean is
+      if (oceanLength == undefined) {
+          oceanLength = 10
+      }
+
+      let oceanWidth = parseInt(ocean.getAttribute('data-oceanWidth')); // how many elements wide the ocean is
+      if (oceanWidth == undefined) {
+          oceanWidth = 20
+      }
+
+      let waveHeight = parseFloat(ocean.getAttribute('data-waveHeight')); // how high each element can rise as part of a wave
+      if (waveHeight == undefined) {
+          waveHeight = 0.5
+      }
+
+      // how wide and deep every element is
+      // smaller elements create finer defined ocean at the cost of computing
+      let oceanResolution = parseFloat(ocean.getAttribute('data-oceanResolution'));
+      if (oceanResolution == undefined) {
+          oceanResolution = 1
+      }
+
+      fillOcean(ocean, oceanLength, oceanWidth, waveHeight, oceanResolution);
+  }
+
+  main();
