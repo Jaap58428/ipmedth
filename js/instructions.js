2af3493abb103164d6897e0a3ceb6f93a964be4c
Dirk van Delft <ROCY6ije>
2019-06-04 17:46:34 +0200

    added first 2 parts of instruction page. [basic vr safety, uitleg van vrijblijvendheid]

--- js/instructions.js
+++ js/instructions.js
@@ -9,3 +9,54 @@ startInstructions = () => {
 }
 
 startInstructions();
+
+
+
+var room = document.querySelector(".room");
+var headset = document.querySelector(".headset");
+var instructionStep = 0;
+document.getElementById('nextButton').addEventListener('click', () => {
+  switch (instructionStep) {
+    case 0:
+      room.style.display = "none";
+      headset.style.display = "block";
+      instructionStep++;
+      break;
+    case 1:
+      room.style.display = "none";
+      headset.style.display = "none";
+      instructionStep++;
+      break;
+    case 2:
+      room.style.display = "none";
+      headset.style.display = "none";
+      break;
+    default:
+      room.style.display = "block";
+      headset.style.display = "none";
+      instructionStep++;
+  }
+})
+
+document.getElementById('prevButton').addEventListener('click', () => {
+  switch (instructionStep) {
+    case 0:
+      room.style.display = "block";
+      headset.style.display = "none";
+      break;
+    case 1:
+      room.style.display = "block";
+      headset.style.display = "none";
+      instructionStep--;
+      break;
+    case 2:
+      room.style.display = "none";
+      headset.style.display = "block";
+      instructionStep--;
+      break;
+    default:
+      room.style.display = "block";
+      headset.style.display = "none";
+      instructionStep++;
+  }
+})
