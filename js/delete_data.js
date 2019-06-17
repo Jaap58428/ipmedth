d90061d21a9261c56af5a9141d1d0184e7523dbe
Hinako <s1103171@student.hsleiden.nl>
2019-06-17 15:00:45 +0200

    .

--- js/delete_data.js
+++ js/delete_data.js
@@ -1,5 +1,7 @@
+/* Autor: Jaap Kanbier (2019) */
 startSettings = () => {
 
+    // add clicklisteners to buttons
     document.getElementById('deleteDataButton').addEventListener('click', () => {
         stateController.deleteLocalStorage();
         stateController.changeState(0);
